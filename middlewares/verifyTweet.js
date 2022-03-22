a0a3413c2c61ae16d9bc5052f54b24146be8018c
Nicolas <pnwa@icloud.com>
2022-03-22 19:56:25 +0100

    content validation middleware

---
+++ middlewares/verifyTweet.js
@@ -0,0 +1,33 @@
+const { object, string } = require("yup")
+const errors = {
+  content: {
+    length: "Your tweet must have between 1 and 140 characters"
+  }
+}
+const tweetContentSchema = object({
+  content: string()
+    .required()
+    .min(1, errors.content.length)
+    .max(140, errors.content.length)
+
+})
+const validateContent = async (req, res, next) => {
+  const { content } = req.body
+
+  try {
+    req.body = await tweetContentSchema.validate(
+      { content },
+      { abortEarly: false }
+    )
+    next()
+  } catch (err) {
+    return res.status(400).send({
+      erreurs: err.errors,
+    })
+  }
+}
+const verifySignup = {
+  validateContent,
+}
+
+module.exports = verifySignup
--- routes/tweet.js
+++ routes/tweet.js
@@ -1,9 +1,10 @@
 const router = require("express").Router()
 const tweetController = require("../controllers/tweet")
 const authJwt = require("../middlewares/authJwt")
+const verifyTweet = require("../middlewares/verifyTweet")
 
 router.get("/feed", authJwt.verifyToken, tweetController.getMyFeed)
 router.get("/:id", tweetController.getTweetById)
-router.post("/", authJwt.verifyToken, tweetController.tweet)
+router.post("/", authJwt.verifyToken, verifyTweet.validateContent, tweetController.tweet)
 
 module.exports = router
