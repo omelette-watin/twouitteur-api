1772bc7921e58449ff307ccaa943197f963f86a7
Nicolas <pnwa@icloud.com>
2022-03-22 21:05:54 +0100

    personnalize required error for content

--- middlewares/verifyTweet.js
+++ middlewares/verifyTweet.js
@@ -1,12 +1,13 @@
 const { object, string } = require("yup")
 const errors = {
   content: {
+    required: "Tell us something unique !",
     length: "Your tweet must have between 1 and 140 characters"
   }
 }
 const tweetContentSchema = object({
   content: string()
-    .required()
+    .required(errors.content.required)
     .min(1, errors.content.length)
     .max(140, errors.content.length)
 
