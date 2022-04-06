68ab789ad34994fe6a950d25126353a265e4ef60
Nicolas <pnwa@icloud.com>
2022-04-06 12:26:12 +0200

    changed origin allowed to local network or env var

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("dotenv").config()
 const app = express()
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONT_URL || "http://192.168.1.72:3000",
     credentials: true,
   })
 )
